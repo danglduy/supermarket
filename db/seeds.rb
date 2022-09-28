e0bb6265d799abfe7a56275b1f0631994ff87b40
danglduy <38782010+danglduy@users.noreply.github.com>
2022-09-28 11:38:35 +0700

    Seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+products_data = [
+  {sku: "A", unit_price: 50},
+  {sku: "B", unit_price: 30},
+  {sku: "C", unit_price: 20},
+  {sku: "D", unit_price: 15}
+]
+
+Product.upsert_all(products_data)
